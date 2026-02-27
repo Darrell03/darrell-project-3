1c28d07eabfee1bb72e51e65f95f9b73290d1d73
Darrell Lee <leedar@gmail.com>
2026-02-27 10:24:38 -0800

    Had to declare 2 variable
    let map;
    let geocoder;

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,5 @@
-
+let map; 
+let geocoder;
 
 function initMap() {
   const myLatLng = { lat: -25.363, lng: 131.044 };
@@ -52,22 +53,4 @@ function handleSearch(address) {
   });
 }
 
-/*window.initMap = initMap;
-
-/* Marker for Bentonville, AR. Home of Walmart! 
-
-function initMap() {
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
-    center: { lat: -33, lng: 151 },
-  });
-  const image =
-    "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png";
-  const beachMarker = new google.maps.Marker({
-    position: { lat: -33.89, lng: 151.274 },
-    map,
-    icon: image,
-  });
-}
-
-window.initMap = initMap;*/
+/*window.initMap = initMap;*/
