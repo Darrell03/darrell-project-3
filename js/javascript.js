c403a9689b6e2b5d46ab546e3ef7d104642323aa
Darrell Lee <leedar@gmail.com>
2026-02-26 16:25:15 -0800

    Adding JS and Sample map code

---
+++ js/javascript.js
@@ -0,0 +1,35 @@
+
+
+function initMap() {
+  const myLatLng = { lat: -25.363, lng: 131.044 };
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: myLatLng,
+  });
+
+  new google.maps.Marker({
+    position: myLatLng,
+    map,
+    title: "Hello World!",
+  });
+}
+
+window.initMap = initMap;
+
+/* Marker for Bentonville, AR. Home of Walmart! */
+
+function initMap() {
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 4,
+    center: { lat: -33, lng: 151 },
+  });
+  const image =
+    "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png";
+  const beachMarker = new google.maps.Marker({
+    position: { lat: -33.89, lng: 151.274 },
+    map,
+    icon: image,
+  });
+}
+
+window.initMap = initMap;
